86c19313650e37d644d052bcf0488eaf08827987
jienhui <laoji52125@163.com>
2020-06-22 16:25:03 +0800

    默认环境为开发

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,9 +4,9 @@ App({
 
     wx.cloud.init({
       // 生产
-      env: 'jike-v2-hnr1l',
+      // env: 'jike-v2-hnr1l',
       // 开发
-      // env: 'jike-mr6e0',
+      env: 'jike-mr6e0',
       traceUser: true,
     })
 
